14de2d95364df2fa9f691d75ede9ec0ac0c322b5
SJLawson <samuel@coherentautomation.com>
2024-10-19 12:27:09 -0400

    docker setup

--- liine_cooks/restaurants/views.py
+++ liine_cooks/restaurants/views.py
@@ -9,7 +9,7 @@ from .serializers import RestaurantHourSerializer
 class RestaurantHourListApiView(APIView):
 
     def get(self, request, *args, **kwargs):
-        request_datetime = request.query_params['datetime'])
+        request_datetime = request.query_params['datetime']
         # str: 2024-10-18T17:30
         restaurant_hours = RestaurantHour.objects.all()
         serializer = RestaurantHourSerializer(restaurant_hours, many=True)
